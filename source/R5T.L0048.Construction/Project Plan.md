1e7d84fcd503f8e377d727d0f06fc0f97fe1e63c
davidcoats <david@getsafetycone.com>
2023-06-12 11:38:51 -0700

    Initial commit.

---
+++ source/R5T.L0048.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.L0048.Construction
+Construction console project for the R5T.L0048 library.
---
+++ source/R5T.L0048/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0048
+{
+	/// <summary>
+	/// Roslyn syntax operations library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
